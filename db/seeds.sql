dad8beeafef025abcd0d2fa30bfbbb18e2b92cd9
Michael Haber <michaeladam.haber@gmail.com>
2020-08-07 16:47:39 -0400

    Set up the templates

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ("Cheeseburger");
+INSERT INTO burgers (name) VALUES ('Chicken burger');
+INSERT INTO burgers(name) VALUES ("Bobby's burger");
+INSERT INTO burgers (name) VALUES ('Mike Burger');
+INSERT INTO burgers (name) VALUES ('Haber Burger');
+INSERT INTO burgers (name) VALUES ('Turkey Burger');

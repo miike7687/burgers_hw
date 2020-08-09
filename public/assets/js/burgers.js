df7f1c9d33dd04a92de4e56bd8a0eecec937e259
Michael Haber <michaeladam.haber@gmail.com>
2020-08-09 16:46:14 -0400

    Added Delete functionality and changed some styling

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -65,4 +65,17 @@ $(function () {
       location.reload();
     });
   });
+
+  $(".delburger").on("click", function (event) {
+    var id = $(this).data("burgerid");
+    console.log(id);
+
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE",
+    }).then(function () {
+      console.log("deleted id ", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
 });

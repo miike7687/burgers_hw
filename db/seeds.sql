67aeac1828d173904db2a2867b1e54d0a9111976
Michael Haber <michaeladam.haber@gmail.com>
2020-08-09 22:07:55 -0400

    Made some small edits

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (name) VALUES ("Cheeseburger");
-INSERT INTO burgers (name) VALUES ('Chicken burger');
-INSERT INTO burgers(name) VALUES ("Bobby's burger");
-INSERT INTO burgers (name) VALUES ('Mike Burger');
-INSERT INTO burgers (name) VALUES ('Haber Burger');
-INSERT INTO burgers (name) VALUES ('Turkey Burger');
+INSERT INTO burgers(name, ingredients)VALUES("Cheeseburger", "Cheese, meat, lettuce, tomato");
+INSERT INTO burgers(name, ingredients)VALUES("Chicken Burger", "Chicken patty, lettuce, tomato, onion");
+INSERT INTO burgers(name, ingredients)VALUES("Bobby's Burger", "Cheese, meat, lettuce, tomato, potato chips");
+INSERT INTO burgers(name, ingredients)VALUES("Mike Burger", "Cheese, meat, lettuce, tomato, bacon,sausage");
+INSERT INTO burgers(name, ingredients)VALUES("Haber Burger", "Cheese, meat, BBQ sauce, turkey bacon");
+INSERT INTO burgers(name, ingredients)VALUES("Turkey Burger", "Turkey patty, lettuce, tomato, onion, bacon");
